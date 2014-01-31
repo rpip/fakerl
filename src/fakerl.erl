16f31376f42da5c77aac19a559500ae8b26ac6ce
Mawuli Adzaku <mawuli@mawuli.me>
2014-01-31 08:40:22 +0000

    Move all fakerl_base_provider functions into fakerl.erl
    Also delete fakerl_base_provider.erl

--- src/fakerl.erl
+++ src/fakerl.erl
@@ -12,7 +12,7 @@
 -compile([export_all]).
 
 name() ->
-   undefined.
+    undefined.
 
 address() ->
     undefined.
@@ -20,20 +20,49 @@ address() ->
 text() ->
     undefined.
 
+%% @doc Returns a random number betweeen 0 and 10
+-spec random_number() -> integer().
 random_number() ->
-    fakerl_base_provider:random_number().
+    random(0,10).
 
+%% @doc Returns a random alphabetic character
+-spec random_letter() -> char().
 random_letter() ->
-    fakerl_base_provider:random_letter().
+    Int = random($a, $z),
+    [Int].
 
+%% @doc Returns a random uppercase alphabetic character
+-spec random_letter(caps) -> char().
 random_letter(caps) ->
-    fakerl_base_provider:random_letter(caps).
+    Int = random($A, $Z),
+    [Int].
 
-random_element(L) ->
-    fakerl_base_provider:random_element(L).
+%% @doc Returns a randomly selected element from the list provided.
+-spec random_element(list()) -> integer().
+random_element([X|[]]) ->
+    X;
+random_element(Xs) ->
+    N = random(1,length(Xs)),
+    lists:nth(N, Xs).
 
+%% @doc Returns a randomly selected number between From, and To.
+%% Uses the default macros: LOWER_BOUND, and UPPER_BOUND for the test data generated range
+-spec random(From, To) -> RandomElement when
+      From :: integer(),
+      To :: integer(),
+      RandomElement :: any().     
 random(From, To) ->
-    fakerl_base_provider:random(From, To).
+    random(From, To, ?LOWER_BOUND, ?UPPER_BOUND).
 
+%% @doc Returns a randomly selected number between From, and To.
+%% The LowerBound and UpperBound represent the range of the generated test data.
+-spec random(From, To, LowerBound, UpperBound) -> RandomElement when
+      From :: integer(),
+      To :: integer(),
+      LowerBound :: integer(),
+      UpperBound :: integer(),
+      RandomElement :: any().     
 random(From, To, LowerBound, UpperBound) ->
-    fakerl_base_provider:random(From, To, LowerBound, UpperBound).
+    L = [crypto:rand_uniform(From, To) || _ <- lists:seq(LowerBound, UpperBound)],
+    lists:nth(1, L).
+

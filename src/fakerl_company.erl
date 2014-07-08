b3cf69681cc0f8c4b73242393a392a426dbe77be
Mawuli Adzaku <mawuli@mawuli.me>
2014-07-08 03:14:57 +0000

    Add docs and type specs

--- src/fakerl_company.erl
+++ src/fakerl_company.erl
@@ -14,21 +14,44 @@
          bs/0
         ]).
 
-
+%% @doc Retruns a full company name
+-spec name() -> string().
 name() ->
     fakerl:parse("company.name").
 
+%% @doc Returns a company name suffix
+%% Examples: Ltd, Inc, Group
+-spec suffix() -> string().
 suffix() ->
     fakerl:fetch("company.suffix").
 
+%% @doc Returns a string of buzzwords.
+-spec buzzwords() -> string().
 buzzwords() ->
     catch_phrase("buzzwords", ?CATCH_PHRASE_LENGTH).
 
+%% @doc Returns a string totally nonsensical words.
+-spec bs() -> string().
 bs() ->
     catch_phrase("bs", ?CATCH_PHRASE_LENGTH).
 
+
+%% @doc Returns a catch of length Length from the given section
+%% under the company node of the locale file.
+-spec catch_phrase(Section, Length) -> string when
+      Section :: string(),
+      Length  :: integer().
 catch_phrase(Section, Length) ->
-    Words = fakerl:fetch("company." ++ Section, all),
+    MaxNum = Length * fakerl:random_number(),
+    Words = [ 
+              fakerl:fetch("company." ++ Section) 
+              || _X <- lists:seq(1, MaxNum)
+            ],
     Words1 = fakerl:shuffle(Words),
     Words2 = lists:sublist(Words1, Length),
-    string:join(Words2, " ").
+    case string:join(lists:merge(Words2), " ") of
+        [] ->
+            catch_phrase(Section, Length);
+        Phrase ->
+            Phrase
+    end.

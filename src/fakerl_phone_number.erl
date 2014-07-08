733d21c873614bf63532a65d09c140cdc6f81e33
Mawuli Adzaku <mawuli@mawuli.me>
2014-07-08 03:18:20 +0000

    Document phone nmber generator

--- src/fakerl_phone_number.erl
+++ src/fakerl_phone_number.erl
@@ -7,7 +7,11 @@
 %%% Created :  8 Feb 2014 by Mawuli Adzaku <mawuli@mawuli.me>
 %%%-------------------------------------------------------------------
 -module(fakerl_phone_number).
+
 -export([phone_number/0]).
 
+%% @doc Returns a radomly generated phone number
+%% By default, the generated phone number is a US phone number format
+-spec phone_number() -> string().
 phone_number() ->
     fakerl:fetch("phone_number.number").
